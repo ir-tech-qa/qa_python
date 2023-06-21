0b0973682c153f95682c47e4569f5e4d564a4d04
Svetlana Zolotova <zolotova@ir-tech.ru>
2023-06-21 10:37:37 +0400

    Исправление замечаний после ревью

--- tests.py
+++ tests.py
@@ -1,11 +1,8 @@
 from main import BooksCollector
 
-
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+import pytest
 
 class TestBooksCollector:
-#Тест на добавление двух книг
     def test_add_new_book_add_two_books(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
@@ -14,42 +11,29 @@ class TestBooksCollector:
 
         assert len(collector.get_books_rating()) == 2
 
-    import pytest
-# Тест проверяет, что можно добавить книге любой рейтинг  от 1 до 10ти
-    @pytest.mark.parametrize('rating', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    def test_set_book_rating_add_rating(self,rating):
+    def test_set_book_rating_add_rating(self):
 
         collector = BooksCollector()
         collector.add_new_book('Черный обелиск')
-        collector.set_book_rating('Черный обелиск', rating)
+        collector.set_book_rating('Черный обелиск', 5)
 
-        assert collector.get_book_rating('Черный обелиск') == rating
+        assert collector.get_book_rating('Черный обелиск') == 5, 'Рейтинг не соответствует ожидаемому'
 
-# Тест проверяет, что нельзя установить рейтинг выше 10
-    def test_set_book_rating_add_rating_over_10(self):
+    @pytest.mark.parametrize('rating', [11, 0])
+    def test_set_book_rating_add_rating_over_10_and_less_1(self,rating):
 
         collector = BooksCollector()
         collector.add_new_book('Тихий Дон')
-        collector.set_book_rating('Тихий Дон', 11)
+        collector.set_book_rating('Тихий Дон', rating)
 
         assert collector.get_book_rating('Тихий Дон') == 1
 
-# Тест проверяет, что нельзя установить рейтинг ниже 1
-    def test_set_book_rating_add_rating_less_1(self):
-            collector = BooksCollector()
-            collector.add_new_book('Евгений Онегин')
-            collector.set_book_rating('Евгений Онегин', 0)
-
-            assert collector.get_book_rating('Евгений Онегин') == 1
-
- # Тест проверяет, что у не добавленной книги нет рейтинга
     def test_get_book_rating_not_book_not_rating(self):
             collector = BooksCollector()
             collector.set_book_rating('Евгений Онегин', 2)
 
             assert collector.get_book_rating('Евгений Онегин') == None
 
-#Тест проверяет вывод всех книг с одинаковым рейтингом
     def test_get_books_with_specific_rating(self):
             collector = BooksCollector()
             collector.add_new_book('Бесы')
@@ -61,7 +45,6 @@ class TestBooksCollector:
 
             assert collector.get_books_with_specific_rating(8) == ['Бесы','Братья Карамазовы']
 
-#Тест на проверку добавления книги в избранное
     def test_add_book_in_favorites(self):
             collector = BooksCollector()
             collector.add_new_book('Цветы для Элджернона')
@@ -69,7 +52,6 @@ class TestBooksCollector:
 
             assert collector.get_list_of_favorites_books() == ['Цветы для Элджернона']
 
-# Тест на проверку добавления одной и той же книги в избранное
     def test_add_book_in_favorites_identical_books(self):
             collector = BooksCollector()
             collector.add_new_book('Цветы для Элджернона')
@@ -78,7 +60,6 @@ class TestBooksCollector:
 
             assert collector.get_list_of_favorites_books() != ['Цветы для Элджернона','Цветы для Элджернона']
 
-# Тест на проверку удаления книги из избранного
     def test_delete_book_from_favorites(self):
             collector = BooksCollector()
             collector.add_new_book('Цветы для Элджернона')
